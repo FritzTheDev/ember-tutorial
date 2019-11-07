ae3a58c469685806f2a5b5037cfffea5eb8ed6c9
Frederick Johnson <fritzj96@gmail.com>
2019-11-07 13:05:45 -0800

    added a new route

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -4,10 +4,24 @@ import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
+  
+  test('should show rentals as the home page', async (assert) => {
 
-  test('visiting /list-rentals', async function(assert) {
-    await visit('/list-rentals');
-
-    assert.equal(currentURL(), '/list-rentals');
   });
+  test('should link to information about the company', async (assert) => {
+
+  });
+  test('should link to contact information', async (assert) => {
+
+  });
+  test('should list available rentals', async (assert) => {
+
+  });
+  test('should filter the list of rentals by city', async (assert) => {
+
+  });
+  test('should show details for a selected rental', async (assert) => {
+
+  });
+
 });
