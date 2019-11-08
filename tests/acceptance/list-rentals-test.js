5a2cb3d695689a1ab6c8f4844a4cc627436b4255
Frederick Johnson <fritzj96@gmail.com>
2019-11-07 21:14:24 -0800

    added test assertion for checking that the appropriate listings are rendered

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -7,22 +7,24 @@ module('Acceptance | list rentals', function(hooks) {
 
   test('should show rentals as the home page', async (assert) => {
     await visit('/');
-    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically.');
   });
   
   test('should link to information about the company', async (assert) => {
     await visit('/');
     await click(".menu-about");
-    assert.equal(currentURL(), '/about', 'should navigate to about');
+    assert.equal(currentURL(), '/about', 'should navigate to about.');
   });
 
   test('should link to contact information', async (assert) => {
     await visit('/');
     await click(".menu-contact");
-    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+    assert.equal(currentURL(), '/contact', 'should navigate to contact.');
   });
   
   test('should list available rentals', async (assert) => {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings.')
 
   });
   test('should filter the list of rentals by city', async (assert) => {
