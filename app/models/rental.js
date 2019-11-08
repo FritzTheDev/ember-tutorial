ff167d3bcb3bd0b76a91fcbbd553a68a8a4f6c39
Frederick Johnson <fritzj96@gmail.com>
2019-11-08 13:00:28 -0800

    created basic model for rentals

--- app/models/rental.js
+++ app/models/rental.js
@@ -2,5 +2,10 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
-
+  title: DS.attr(),
+  owner: DS.attr(),
+  city: DS.attr(),
+  category: DS.attr(),
+  image: DS.attr(),
+  bedrooms: DS.attr()
 });
