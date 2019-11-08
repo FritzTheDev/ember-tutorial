5f667aed860b43b0fbf3729b10c42c9ec626fd63
Frederick Johnson <fritzj96@gmail.com>
2019-11-07 21:24:37 -0800

    added new test & changed arrow functions to normal functions

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -5,27 +5,26 @@ import { click, currentURL, visit } from '@ember/test-helpers';
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('should show rentals as the home page', async (assert) => {
+  test('should show rentals as the home page', async function (assert) {
     await visit('/');
     assert.equal(currentURL(), '/rentals', 'should redirect automatically.');
   });
   
-  test('should link to information about the company', async (assert) => {
+  test('should link to information about the company', async function (assert) {
     await visit('/');
     await click(".menu-about");
     assert.equal(currentURL(), '/about', 'should navigate to about.');
   });
 
-  test('should link to contact information', async (assert) => {
+  test('should link to contact information', async function (assert) {
     await visit('/');
     await click(".menu-contact");
     assert.equal(currentURL(), '/contact', 'should navigate to contact.');
   });
   
-  test('should list available rentals', async (assert) => {
+  test('should list available rentals', async function (assert) {
     await visit('/');
-    console.log(this)
-    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings.')
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
 
   });
   test('should filter the list of rentals by city', async (assert) => {
