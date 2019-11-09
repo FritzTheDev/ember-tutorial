ee461422a7ee6abf4345cd25fb8c18c7d10c7950
Frederick Johnson <fritzj96@gmail.com>
2019-11-08 19:08:51 -0800

    progress on test

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, settled, triggerKeyEvent, fillIn } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 const ITEMS = [{ city: 'San Francisco'}, { city: 'Portland' }, { city: 'Seattle' }];
@@ -11,5 +11,20 @@ module('Integration | Component | list-filter', function(hooks) {
 
   test('should initially load all listings', async function (assert) {
     this.set('filterByCity', () => Promise.resolve({ results: ITEMS }));
-  })
+    await render(hbs`
+    <ListFilter @filter={{action filterByCity}} as |results|>
+      <ul>
+        {{#each results as |item|}}
+          <li class="city">
+            {{item.city}}
+          </li>
+      </ul>
+    </ListFilter>
+    `);
+
+    await settled();
+
+    assert.equal(this.element.querySelectorAll('.city').length, 3);
+    assert.dom(this.element.querySelector('.city').hasText('San Francisco'));
+  });
 });
